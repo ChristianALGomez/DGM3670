09a55f7e93312dd6334612b467ebdc51e996ce86
Christian_Gomez <10721401@my.uvu.edu>
2019-11-14 10:56:28 -0700

    Calculator_PythonV2

--- Python_DGM_3670/Calculator.py
+++ Python_DGM_3670/Calculator.py
@@ -13,6 +13,9 @@ def Add(values):
         sum += val
 
     return sum
+Add([])
+
+    #Add([])
 
     #sum = value1 + value2
     #return sum
@@ -20,15 +23,47 @@ def Add(values):
     #return value1 + value2
 
 
-    sum=0
-    for val in values:
-        if type(val) == 'int' or type(val) == 'float':
-            sum += val
-        else:
-            print "your a dummy. you cant add", val
-
-    return sum
-
 def Power(value,power):
     import math
-    return math.pow(value, power )
+    return math.pow(value,power)
+Power()
+
+
+
+def Sub(values):
+    sum = values[0]
+    for val in values[1:]:
+        sum -= val
+    return sum
+Sub([])
+
+def Div(values):
+    sum = values[0]
+    for val in values[1:]:
+        sum = sum / val
+    return sum
+Div([])
+
+def Mult(values):
+    sum = values[0]
+    for val in values[1:]:
+        sum *= val
+    return sum
+Mult([])
+
+def Med(values):
+    import math
+    numorder = sorted(values)
+    size = len(numorder)
+
+    if size / 2 !=0:
+        temp = math.floor(size/2)
+        median = numorder[temp]
+
+    if size % 2 ==0:
+        temp = size/2
+        medianalt = temp-1
+
+        median =(numorder[temp] + numorder[medianalt])
+        median = median/2
+    return median
